8cc9871ef6013dd615b90350b1402040187a13e0
Tatsuhiko Kimita <nannbo@crappo.net>
2022-09-12 17:20:34 +0900

    認証チェックをAOPに置き換え

--- src/Resource/Page/Next.php
+++ src/Resource/Page/Next.php
@@ -4,39 +4,17 @@ declare(strict_types=1);
 
 namespace Cw\LearnBear\Resource\Page;
 
-use BEAR\Resource\Code;
-use BEAR\Resource\RenderInterface;
-use BEAR\Resource\ResourceObject;
 use BEAR\Sunday\Inject\ResourceInject;
-use Cw\LearnBear\AppSpi\SessionHandlerInterface;
-use Ray\Di\Di\Named;
+use Cw\LearnBear\Annotation\CheckAuth;
+use Cw\LearnBear\Resource\AuthBaseResourceObject;
 
-class Next extends ResourceObject
+class Next extends AuthBaseResourceObject
 {
     use ResourceInject;
 
-    public function __construct(
-        #[Named('error_page')] private readonly RenderInterface $errorRender,
-        private readonly SessionHandlerInterface $cwSession,
-    ) {
-    }
-
+    #[CheckAuth]
     public function onGet(int $year, int $month, int $day): static
     {
-        // 認証されたPHPセッション継続をチェック
-        if ($this->cwSession->isNotAuthorized()) {
-            $this->code = Code::UNAUTHORIZED;
-            $this->setRenderer($this->errorRender);
-            $this->body = [
-                'status' => [
-                    'code' => $this->code,
-                    'message' => 'ユーザー認証をしてください',
-                ],
-            ];
-
-            return $this;
-        }
-
         $params = ['year' => $year, 'month' => $month, 'day' => $day];
         $this->body = $params + [
             'weekday' => $this->resource->get('app://self/weekday', $params),
