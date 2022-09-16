d4e4ca32781505f47f340472a4786f6030ed9ee0
Tatsuhiko Kimita <nannbo@crappo.net>
2022-09-16 19:38:33 +0900

    入力フォームの作成
    1. フォームクラスの作成
        - 直近のBEAR.Sundayパッケージの composer tests を暫定的にパスさせるために、いくつか psalm や phpstan を抑止している。
        - 併せて作成している QueryForNext は Polidog.Todo と無関係だが、画面遷移時に標準チュートリアルに関連するコード部分に対処するために追加している。
    2. AppModuleにFormクラスを記述する
    3. Nextリソース変更
        - 継承元(認証Baseクラス)でコンストラクタインジェクションを定義済みなので、ここでは別の手段でDIする必要がある。
        - Ray.Di(https://ray-di.github.io/manuals/1.0/ja/injections.html)のセッターインジェクションで代替している。
    4. URIの指定
    5. データの渡し方

--- src/Module/HtmlModule.php
+++ src/Module/HtmlModule.php
@@ -4,9 +4,12 @@ declare(strict_types=1);
 
 namespace Cw\LearnBear\Module;
 
+use Cw\LearnBear\Infrastructure\Form\TodoForm;
 use Madapaja\TwigModule\TwigErrorPageModule;
 use Madapaja\TwigModule\TwigModule;
 use Ray\Di\AbstractModule;
+use Ray\WebFormModule\AuraInputModule;
+use Ray\WebFormModule\FormInterface;
 
 class HtmlModule extends AbstractModule
 {
@@ -14,5 +17,8 @@ class HtmlModule extends AbstractModule
     {
         $this->install(new TwigModule());
         $this->install(new TwigErrorPageModule());
+        $this->install(new AuraInputModule());
+        $this->bind(TodoForm::class);
+        $this->bind(FormInterface::class)->annotatedWith('todo_form')->to(TodoForm::class);
     }
 }
